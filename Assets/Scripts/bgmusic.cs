c651be61d07b1c21e0a6f75d8f7de5f21809c519
Levi Batkin <90899332+Levi-Batkin@users.noreply.github.com>
2022-02-25 11:20:08 +0000

    updated

--- Assets/Scripts/bgmusic.cs
+++ Assets/Scripts/bgmusic.cs
@@ -8,20 +8,55 @@ public class bgmusic : MonoBehaviour
     public AudioSource puzzleMusic;
     [SerializeField] Slider music;
     public GameObject musicon, musicoff;
+    int musickey;
+    float musicplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(!PlayerPrefs.HasKey("musickey"))
+		{
+            Debug.Log("Music has been defaultly set to 1");
+			PlayerPrefs.SetFloat("musickey", 1);
+            musicon.SetActive(true);
+            musicoff.SetActive(false);
+		}
+		else
+		{
+			musicplay = PlayerPrefs.GetFloat("musickey");
+            if(musicplay != 1) 
+            {
+                Debug.Log("Music is currently inactive.");
+                musicon.SetActive(false);
+                musicoff.SetActive(true);
+            }
+            else{
+                Debug.Log("Music is currently active.");
+                musicon.SetActive(true);
+                musicoff.SetActive(false);
+            }
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (musicon.activeSelf) {
+        if (musicon.activeSelf) 
+        {
+            Debug.Log("Music is currently active.");
             puzzleMusic.volume = music.value;
+            musicon.SetActive(true);
+            musicoff.SetActive(false);
         }
-        else{
+        else
+        {   
+            Debug.Log("Music is currently inactive.");
             puzzleMusic.volume = 0;
+            if(PlayerPrefs.GetFloat("musickey") != 1)
+            {
+                PlayerPrefs.SetFloat("musickey", 0);
+                musicon.SetActive(false);
+                musicoff.SetActive(true);
+            }
         }
     }
 }
